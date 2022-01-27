bd442a510968d056fc4db981f66f9c60dd3a9163
LoFAYiA1ZSc5aIx1 <98504989+LoFAYiA1ZSc5aIx1@users.noreply.github.com>
2022-01-27 04:25:45 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from datetime import datetime
+
+i = int(datetime.now().strftime('%d'))
+j = int(datetime.now().strftime('%m'))
+h = i-10
+b = 0
+c = []
+d = []
+g = []
+
+while h != i+1:
+    a = requests.get('https://checkerproxy.net/api/archive/2022-{}-{}'.format(j,str(h))).text
+    json_data = json.loads(a)
+    for x in json_data:
+        if json_data[b]['type'] == 1:
+            c.append(json_data[b]['addr'])
+        elif json_data[b]['type'] == 2:
+            d.append(json_data[b]['addr'])
+        elif json_data[b]['type'] == 4:
+            g.append(json_data[b]['addr'])
+        else:
+            print('wtf')
+        b = b + 1
+    h = h + 1
+    b = 0
+
+with open('http.txt','w') as f:
+    for x in c:
+        f.write(x+'\n')
+
+with open('https.txt','w') as f:
+    for x in d:
+        f.write(x+'\n')
+
+with open('socks5.txt','w') as f:
+    for x in g:
+        f.write(x+'\n')
