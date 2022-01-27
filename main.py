683093fa656370a1b0cc5633dafcfd7144e7b3cc
LoFAYiA1ZSc5aIx1 <98504989+LoFAYiA1ZSc5aIx1@users.noreply.github.com>
2022-01-27 17:45:56 +0100

    Update main.py
    remove broken proxies

--- main.py
+++ main.py
@@ -10,26 +10,44 @@ b = 0
 c = []
 d = []
 g = []
+m = []
 
 while h != i+1:
     a = requests.get('https://checkerproxy.net/api/archive/{}-{}-{}'.format(k,j,str(h))).text
     json_data = json.loads(a)
     for x in json_data:
-        if json_data[b]['type'] == 1:
-            c.append(json_data[b]['addr'])
-        elif json_data[b]['type'] == 2:
-            d.append(json_data[b]['addr'])
-        elif json_data[b]['type'] == 4:
-            g.append(json_data[b]['addr'])
+        if x['ip'] in ['127.0.0.1','172.18.0.1']:
+            m.append(x['addr'])
         else:
-            print('garbage value')
-        b = b + 1
+            if json_data[b]['type'] == 1:
+                c.append(json_data[b]['addr'])
+            elif json_data[b]['type'] == 2:
+                d.append(json_data[b]['addr'])
+            elif json_data[b]['type'] == 4:
+                g.append(json_data[b]['addr'])
+            else:
+                print('garbage value')
+            b = b + 1
     h = h + 1
     b = 0
 
 c = list(dict.fromkeys(c))
 d = list(dict.fromkeys(d))
 g = list(dict.fromkeys(g))
+
+for x in m:
+    try:
+        c.remove(x)
+    except:
+        pass
+    try:
+        d.remove(x)
+    except:
+        pass
+    try:
+        g.remove(x)
+    except:
+        pass
 
 with open('http.txt','w') as f:
     for x in c:
